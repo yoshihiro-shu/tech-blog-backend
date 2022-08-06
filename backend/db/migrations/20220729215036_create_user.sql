c09bdd8e902e6487f813a826883054da5b50ea6a
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2022-08-06 22:50:38 +0900

    add test data

--- backend/db/migrations/20220729215036_create_user.sql
+++ backend/db/migrations/20220729215036_create_user.sql
@@ -8,6 +8,10 @@ CREATE TABLE users (
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
+-- password=password
+INSERT INTO users (name, password, email) VALUES("test1", "$2a$10$s0IkhGD3R9qmwZ8/afJbP..uKGPNGl/ObUrVH8J2j181uk0KTfJ3q", "teat1@mail.com");
+INSERT INTO users (name, password, email) VALUES("test2", "$2a$10$s0IkhGD3R9qmwZ8/afJbP..uKGPNGl/ObUrVH8J2j181uk0KTfJ3q", "teat2@mail.com");
+INSERT INTO users (name, password, email) VALUES("test3", "$2a$10$s0IkhGD3R9qmwZ8/afJbP..uKGPNGl/ObUrVH8J2j181uk0KTfJ3q", "teat3@mail.com");
 -- +goose StatementEnd
 
 -- +goose Down
