c44438ab42f2137b2fa7d25910ca5c6f4204f25f
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-13 23:27:26 +0900

    ADD tag

---
+++ migrations/20221013203344_create_tag.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  description varchar(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE article_tag(
+  article_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE article_tag;
+DROP TABLE tags;
+-- +goose StatementEnd
