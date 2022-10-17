94ec416c543e7dce302cdf1770f6dc868d7db999
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-18 00:39:17 +0900

    UPDATE GetTopPage which can get data via redis

--- backend/controllers/api/article.go
+++ backend/controllers/api/article.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 
+	article_cache "github.com/yoshihiro-shu/draft-backend/model/article/cache"
 	article_linkages_to_many "github.com/yoshihiro-shu/draft-backend/model/article/linkages/to/many"
 	"github.com/yoshihiro-shu/draft-backend/request"
 )
@@ -13,12 +14,20 @@ type articleHandler struct {
 
 func (a articleHandler) GetTopPage(w http.ResponseWriter, r *http.Request) error {
 
+	var acs []article_linkages_to_many.Article
+	err := a.C.Cache.GET(article_cache.TopPageAritcleListKey, &acs)
+	if err == nil {
+		return a.C.JSON(w, http.StatusOK, acs)
+	}
+
 	ac := &article_linkages_to_many.Article{}
 
 	articles, err := ac.GetArticleList(a.C.DB())
 	if err != nil {
 		return a.C.JSON(w, http.StatusInternalServerError, err.Error())
 	}
+
+	err = a.C.Cache.SET(article_cache.TopPageAritcleListKey, articles)
 	return a.C.JSON(w, http.StatusOK, articles)
 }
 
