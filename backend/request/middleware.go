7cddebbe57989d034af1d28467253294db1361fd
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-23 04:18:49 +0900

    add header set info for cors header error

--- backend/request/middleware.go
+++ backend/request/middleware.go
@@ -7,6 +7,17 @@ import (
 
 func (c Context) TestMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// response for preflight
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// TODO
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 		c.Logger.Println("url:", r.URL)
 		fmt.Fprintln(w, "url:", r.URL)
 		next.ServeHTTP(w, r)
