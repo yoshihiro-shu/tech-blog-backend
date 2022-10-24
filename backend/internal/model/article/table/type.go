c8f8073e26c02f9450dc527ebb19bd468e35c401
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-24 22:52:37 +0900

    fix struct

--- backend/internal/model/article/table/type.go
+++ backend/internal/model/article/table/type.go
@@ -3,14 +3,15 @@ package table
 import "time"
 
 type Article struct {
-	Id         int       `json:"id"`
-	UserId     int       `json:"userId"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	Status     int       `json:"status"`
-	CreatedAt  time.Time `json:"createdAt"`
-	UpdatedAt  time.Time `json:"updatedAt"`
-	CategoryId int       `json:"categoryId"`
+	Id           int       `json:"id"`
+	UserId       int       `json:"userId"`
+	ThumbnailUrl string    `json:"thumbnailUrl"`
+	Title        string    `json:"title"`
+	Content      string    `json:"content"`
+	Status       int       `json:"status"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+	CategoryId   int       `json:"categoryId"`
 }
 
 const (
