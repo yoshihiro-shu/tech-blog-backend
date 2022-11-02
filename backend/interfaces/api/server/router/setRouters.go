e624bdc9c1e3eb0e8a91fa4792c404ab59a5c9e3
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-11-03 03:36:04 +0900

    refactor router

--- backend/interfaces/api/server/router/setRouters.go
+++ backend/interfaces/api/server/router/setRouters.go
@@ -17,34 +17,41 @@ func (r Router) ApplyRouters() {
 		Context: ctx,
 	}
 
-	th := api.NewTopPageHandler(ctx)
-
-	r.AppHandle("/", h.Index).Methods(http.MethodGet)
-	r.AppHandle("/top", th.GetTopPage).Methods(http.MethodGet)
-
-	// Grouping
-	t := r.Group("/test")
-	t.AppHandle("", h.TestHandler).Methods(http.MethodGet)
-	t.AppHandle("/redis", h.TestSetRedis).Methods(http.MethodPost)
-	t.AppHandle("/redis/{key}", h.TestGetRedis).Methods(http.MethodGet)
-	t.AppHandle("/v2", h.Index).Methods(http.MethodGet)
-
-	c := r.Group("/cmd")
-	c.AppHandle("", h.Command).Methods(http.MethodGet)
-
-	user := r.Group("/users")
-	user.AppHandle("", h.GetUsers).Methods(http.MethodGet)
-	user.AppHandle("/{id}", h.GetUserBYID).Methods(http.MethodGet)
-	user.AppHandle("/login", h.Login).Methods(http.MethodPost)
-	user.AppHandle("/signup", h.SignUp).Methods(http.MethodPost)
-	// user.HandleFunc("/register", h.RegisterAccount).Methods(http.MethodPost)
-
-	article := r.Group("/articles")
-	article.AppHandle("", h.GetArticles).Methods(http.MethodGet)
-	article.AppHandle("/{id}", h.GetArticleByID).Methods(http.MethodGet)
-
-	a := r.Group("/auth")
-	a.Use(auth.AuthMiddleware)
-	a.AppHandle("/index", h.AuthIndex).Methods(http.MethodGet)
-
+	{
+		r.AppHandle("/", h.Index).Methods(http.MethodGet)
+	}
+	{
+		th := api.NewTopPageHandler(ctx)
+		r.AppHandle("/top", th.GetTopPage).Methods(http.MethodGet)
+	}
+	{
+		// Grouping
+		t := r.Group("/test")
+		t.AppHandle("", h.TestHandler).Methods(http.MethodGet)
+		t.AppHandle("/redis", h.TestSetRedis).Methods(http.MethodPost)
+		t.AppHandle("/redis/{key}", h.TestGetRedis).Methods(http.MethodGet)
+		t.AppHandle("/v2", h.Index).Methods(http.MethodGet)
+	}
+	{
+		c := r.Group("/cmd")
+		c.AppHandle("", h.Command).Methods(http.MethodGet)
+	}
+	{
+		user := r.Group("/users")
+		user.AppHandle("", h.GetUsers).Methods(http.MethodGet)
+		user.AppHandle("/{id}", h.GetUserBYID).Methods(http.MethodGet)
+		user.AppHandle("/login", h.Login).Methods(http.MethodPost)
+		user.AppHandle("/signup", h.SignUp).Methods(http.MethodPost)
+		// user.HandleFunc("/register", h.RegisterAccount).Methods(http.MethodPost)
+	}
+	{
+		article := r.Group("/articles")
+		article.AppHandle("", h.GetArticles).Methods(http.MethodGet)
+		article.AppHandle("/{id}", h.GetArticleByID).Methods(http.MethodGet)
+	}
+	{
+		a := r.Group("/auth")
+		a.Use(auth.AuthMiddleware)
+		a.AppHandle("/index", h.AuthIndex).Methods(http.MethodGet)
+	}
 }
