0ad890c8c8c895b1464755d97db7b19b91fba69f
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-11 16:01:29 +0900

    ADD reprica to article methods

--- backend/registory/article_registory.go
+++ backend/registory/article_registory.go
@@ -1,14 +1,15 @@
 package registory
 
 import (
+	"github.com/go-pg/pg"
 	"github.com/yoshihiro-shu/draft-backend/application/usecase"
 	"github.com/yoshihiro-shu/draft-backend/infrastructure/persistence"
 	"github.com/yoshihiro-shu/draft-backend/interfaces/api/server/handler"
 	"github.com/yoshihiro-shu/draft-backend/interfaces/api/server/request"
 )
 
-func NewArticleRegistory(ctx *request.Context) handler.ArticleHandler {
-	articleRepository := persistence.NewArticlePersistence(ctx.MasterDB())
+func NewArticleRegistory(ctx *request.Context, master, reprica func() *pg.DB) handler.ArticleHandler {
+	articleRepository := persistence.NewArticlePersistence(master, reprica)
 	articleUseCase := usecase.NewArticleUseCase(articleRepository)
 	return handler.NewArticleHandler(articleUseCase, ctx)
 }
