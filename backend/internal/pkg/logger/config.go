3b14c4a8451eba47cbdca9f5d85b416d3237abcf
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-12 21:31:27 +0900

    ADD encoding

--- backend/internal/pkg/logger/config.go
+++ backend/internal/pkg/logger/config.go
@@ -10,4 +10,7 @@ func init() {
 	// TODO config.yamlから設定できるようにする
 	config = zap.NewDevelopmentConfig()
 	config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig = encoderConfig
 }
