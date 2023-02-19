b8d2941ddfc68536b0ef37c117a5f43797ddfa2f
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-19 22:42:46 +0900

    ADD refresh token table

---
+++ migrations/db/20230219165512_create_refresh_tokens.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE refresh_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    jwt_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE refresh_tokens;
+-- +goose StatementEnd
