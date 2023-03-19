f79cb242e5641cec5d845ba2fb0079a5dcecc575
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-03-19 16:17:47 +0900

    separete twitter

--- backend/internal/config/new.go
+++ backend/internal/config/new.go
@@ -29,13 +29,6 @@ type User struct {
 	Port string `yaml:"port"`
 }
 
-type Twitter struct {
-	Apikey       string `yaml:"api_key"`
-	ApiKeySecret string `yaml:"api_key_secret"`
-	BearerToken  string `yaml:"bearer_token"`
-	UserId       string `yaml:"user_id"`
-}
-
 type AuthToken struct {
 	SecretKey string        `yaml:"secret_key"`
 	Expires   time.Duration `yaml:"expires"`
