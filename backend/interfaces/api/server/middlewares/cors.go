cab429fcf3f6744dc2fa365d31518083aac0a614
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-03-21 13:25:28 +0900

    fix middleware name

--- backend/interfaces/api/server/middlewares/cors.go
+++ backend/interfaces/api/server/middlewares/cors.go
@@ -1,10 +1,10 @@
-package middleware
+package middlewares
 
 import (
 	"net/http"
 )
 
-func CorsMiddleware(next http.Handler) http.Handler {
+func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
