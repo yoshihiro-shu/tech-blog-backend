d2595a0543b23df0b5ca60889c73a810e1cfe839
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-03-25 03:47:04 +0900

    delete header utils

--- backend/interfaces/api/server/middlewares/cors.go
+++ backend/interfaces/api/server/middlewares/cors.go
@@ -3,20 +3,17 @@ package middlewares
 import (
 	"net/http"
 
-	"github.com/yoshihiro-shu/draft-backend/backend/interfaces/api/server/request"
 	"github.com/yoshihiro-shu/draft-backend/backend/internal/config"
 )
 
 func Cors(c config.Frontend) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			header := request.NewHeader(w)
-
-			header.ADD("Access-Control-Allow-Origin", c.Domain)
-			header.ADD("Access-Control-Allow-Headers", "*")
-			header.ADD("Access-Control-Allow-Methods", http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions)
-			header.ADD("Content-Type", "application/json;charset=utf-8")
-			// w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Content-Type", "application/json;charset=utf-8")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 			// response for Preflight request
 			if r.Method == http.MethodOptions {
