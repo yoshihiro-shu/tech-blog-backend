c064008ca8c54945baa1354c07b2f2ac688cf1d5
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-06-14 18:54:50 +0900

    ADD mock

---
+++ backend/internal/mock_test/mock_context.go
@@ -0,0 +1,17 @@
+package mock_test
+
+import (
+	"testing"
+
+	"github.com/yoshihiro-shu/draft-backend/backend/interfaces/api/server/request"
+	"github.com/yoshihiro-shu/draft-backend/backend/internal/config"
+	"github.com/yoshihiro-shu/draft-backend/backend/internal/pkg/logger"
+)
+
+func NewContext(t *testing.T) *request.Context {
+	conf := config.Configs{}
+	db, _ := MockDB(t)
+	logger := logger.New()
+	cache := MockRedisClient{}
+	return request.NewContext(conf, logger, db, cache)
+}
