ce524f4c9b68815de1869a44432efadc1fe3cfdb
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-24 00:21:48 +0900

    refactor cache handling

--- backend/interfaces/api/handler/top_page.go
+++ backend/interfaces/api/handler/top_page.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/yoshihiro-shu/draft-backend/backend/application/usecase"
@@ -9,7 +8,6 @@ import (
 	"github.com/yoshihiro-shu/draft-backend/backend/interfaces/api/request"
 	"github.com/yoshihiro-shu/draft-backend/backend/interfaces/cache"
 	"github.com/yoshihiro-shu/draft-backend/backend/internal/logger"
-	"github.com/yoshihiro-shu/draft-backend/backend/internal/pager"
 	"go.uber.org/zap"
 )
 
@@ -38,15 +36,13 @@ func NewTopPageHandler(topPageUseCase usecase.TopPageUseCase, c *request.Context
 
 type responseTopPage struct {
 	Article []model.Article `json:"articles"`
-	Pager   *pager.Pager    `json:"pager"`
 }
 
 func (h topPageHandler) Get(w http.ResponseWriter, r *http.Request) error {
 	currentPage := 1
 	var res responseTopPage
 
-	resKey := fmt.Sprintf(cache.TopPageAritcleListKeyByPage, currentPage)
-
+	resKey := cache.TopPageKey()
 	err := h.Cache().GET(resKey, &res)
 	if err == nil {
 		return h.JSON(w, http.StatusOK, res)
@@ -58,12 +54,6 @@ func (h topPageHandler) Get(w http.ResponseWriter, r *http.Request) error {
 	err = h.topPageUseCase.GetArticles(&res.Article, limit, offset)
 	if err != nil {
 		h.logger.Warn("failed at get articles at top page.", zap.Error(err))
-		return h.JSON(w, http.StatusInternalServerError, err.Error())
-	}
-
-	res.Pager, err = h.topPageUseCase.GetPager(currentPage, numberOfArticlePerPage)
-	if err != nil {
-		h.logger.Warn("failed at get pager at top page.", zap.Error(err))
 		return h.JSON(w, http.StatusInternalServerError, err.Error())
 	}
 
--- backend/interfaces/cache/keys.go
+++ backend/interfaces/cache/keys.go
@@ -1,6 +1,28 @@
 package cache
 
+import "fmt"
+
 const (
-	TopPageAritcleListKey       = "top_page_article_list"
-	TopPageAritcleListKeyByPage = "top_page_%d_article_list"
+	topPageKey = "top_page"
+
+	latestArticleListKey = "latest_article_list_page_%d"
+
+	getArticlesByCategoryKey = "articles_by_category_%s"
+	getArticlesByTagKey      = "articles_by_tag_%s"
 )
+
+func TopPageKey() string {
+	return topPageKey
+}
+
+func GetLatestArticleListKey(page int) string {
+	return fmt.Sprintf(latestArticleListKey, page)
+}
+
+func GetArticlesByCategoryKey(slug string) string {
+	return fmt.Sprintf(getArticlesByCategoryKey, slug)
+}
+
+func GetArticlesByTagKey(slug string) string {
+	return fmt.Sprintf(getArticlesByTagKey, slug)
+}
