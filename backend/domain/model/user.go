3530ebaf2785f806b3bf39f95a7a282d7da21c2e
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-23 21:40:22 +0900

    add: omitempty

--- backend/domain/model/user.go
+++ backend/domain/model/user.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type User struct {
-	Id        int       `gorm:"primaryKey;" json:"id"`
-	Name      string    `json:"name"`
-	Password  string    `json:"password"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"createdAt"`
+	Id        int       `gorm:"primaryKey;" json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
 }
 
 func NewUser(name, password, email string) *User {
